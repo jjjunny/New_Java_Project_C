2b1cac96b426f10e0afb6e79480da9df45d21ce9
jjjunny <gudwns0240@naver.com>
2024-10-27 00:50:41 +0900

    Calculator 클래스를 생성

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class Calculator {
-}
+
+    JTextArea area; // 계산기 화면을 표시할 텍스트 영역
+    // 계산기 버튼들을 선언
+    JButton AC, C, per, devide, seven, eight, nine, multiply, four, five, six, minus,
+            one, two, three, plus, plmi, zero, dot, equal;
