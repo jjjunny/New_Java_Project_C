4ddcfd35f55218f6ec708f33454143ca5269aec9
jjjunny <gudwns0240@naver.com>
2024-10-28 00:37:35 +0900

    계산기의 기본이 되는 멤버 필드 선언

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,18 @@
 import javax.swing.*;
 
-public class Calculator {
+/**
+ * 2024.10.17에 최초 생성(Kim Hyeongjun)
+ * 2024.10.17에 최초 생성이지만 2024.10.27에 브랜치를 잘못 건드려서 2024.10.27에 새로 생성
+ * 처음에 책에 있는 코드를 보면서 응용하여 만듬
+ * 2024.10.18 계산기의 기본이 되는 멤버 필드 선언
+ */
+public class Calculator extends JFrame {
 
     JTextArea area; // 계산기 화면을 표시할 텍스트 영역
     // 계산기 버튼들을 선언
-    JButton AC, C, per, devide, seven, eight, nine, multiply, four, five, six, minus,
-            one, two, three, plus, plmi, zero, dot, equal;
+    JButton AC, C, per, devide, multiply, minus, plus, plmi, dot, equal;
+
+    Calculator() {
+
+    }
+}
